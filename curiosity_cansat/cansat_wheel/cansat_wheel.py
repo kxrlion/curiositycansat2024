69e46ae94cc17bac56e0c0081bf995a44a373e9c
kxrlion <kxrlion@gmail.com>
2024-03-05 15:48:08 +0100

    motor function

--- curiosity_cansat/cansat_wheel/cansat_wheel.py
+++ curiosity_cansat/cansat_wheel/cansat_wheel.py
@@ -1,12 +1,15 @@
+from xml.etree.ElementTree import PI
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
 # Set up GPIO pins
+#left motor
 motorA_in1 = 5
 motorA_in2 = 6
 
+#right motor
 motorB_in1 = 23
 motorB_in2 = 24
 
@@ -14,25 +17,69 @@ GPIO.setup(motorA_in1, GPIO.OUT)
 GPIO.setup(motorA_in2, GPIO.OUT)
 GPIO.setup(motorB_in1, GPIO.OUT)
 GPIO.setup(motorB_in2, GPIO.OUT)
+angle = 285
+spin = 360/angle
+r=3.25;
 
-def move_forward():
-    GPIO.output(motorA_in1, GPIO.HIGH)
-    GPIO.output(motorA_in2, GPIO.LOW)
-    GPIO.output(motorB_in1, GPIO.HIGH)
-    GPIO.output(motorB_in2, GPIO.LOW)
-
-def move_backward():
+def backward(m):
+    #m -> liczba cm do przejechania
+    #n -> ile razy ma sie obrocic kolo
+    n=m/2*PI*r
     GPIO.output(motorA_in1, GPIO.LOW)
     GPIO.output(motorA_in2, GPIO.HIGH)
     GPIO.output(motorB_in1, GPIO.LOW)
     GPIO.output(motorB_in2, GPIO.HIGH)
-
+    
+    time.sleep(n*spin)
+def forward(m):
+    #m -> liczba cm do przejechania
+    #n -> ile razy ma sie obrocic kolo
+    n=m/2*PI*r
+    GPIO.output(motorA_in1, GPIO.HIGH)
+    GPIO.output(motorA_in2, GPIO.LOW)
+    GPIO.output(motorB_in1, GPIO.HIGH)
+    GPIO.output(motorB_in2, GPIO.LOW)
+    time.sleep(n*spin)
+def right(a):
+    #a -> a podane w radianach / PI
+    GPIO.output(motorA_in1, GPIO.HIGH)
+    GPIO.output(motorA_in2, GPIO.LOW)
+    GPIO.output(motorB_in1, GPIO.LOW)
+    GPIO.output(motorB_in2, GPIO.HIGH)
+    time.sleep(a*spin)
+def left(a):
+    #a -> a podane w radianach / PI
+    GPIO.output(motorA_in1, GPIO.LOW)
+    GPIO.output(motorA_in2, GPIO.HIGH)
+    GPIO.output(motorB_in1, GPIO.HIGH)
+    GPIO.output(motorB_in2, GPIO.LOW)
+    time.sleep(a*spin)
+def c():
+    left(1/3)
+    forward(20)
+    right(1/3)
+    forward(20)
+    right(1/3)
+    forward(20)
+    right(1/6)
+def a1():
+    pass
+def n():
+    pass
+def s():
+    pass
+def a2():
+    pass
+def t():
+    pass
 try:
-    move_forward()
-    time.sleep(2)
-
-    move_backward()
-    time.sleep(2)
+    # c -> a1 -> n -> s -> a2 -> t
+    c()
+    a1()
+    n()
+    s()
+    a2()
+    t()
 
 finally:
     GPIO.cleanup()
