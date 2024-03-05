8cd6f88c7b9d5fa1e81e279050fedc83f8554421
kxrlion <kxrlion@gmail.com>
2024-03-05 20:40:37 +0100

    add writing

--- curiosity_cansat/cansat_wheel/cansat_wheel.py
+++ curiosity_cansat/cansat_wheel/cansat_wheel.py
@@ -20,7 +20,8 @@ GPIO.setup(motorB_in2, GPIO.OUT)
 angle = 285
 spin = 360/angle
 r=3.25;
-
+Aconst = 2,136000936
+Nconst = 2,441549178
 def backward(m):
     #m -> liczba cm do przejechania
     #n -> ile razy ma sie obrocic kolo
@@ -54,32 +55,79 @@ def left(a):
     GPIO.output(motorB_in1, GPIO.HIGH)
     GPIO.output(motorB_in2, GPIO.LOW)
     time.sleep(a*spin)
-def c():
+def c(x):
     left(1/3)
-    forward(20)
+    forward(x)
     right(1/3)
-    forward(20)
+    forward(x)
     right(1/3)
-    forward(20)
+    forward(x)
     right(1/6)
-def a1():
-    pass
-def n():
-    pass
-def s():
-    pass
-def a2():
-    pass
-def t():
+def a1(x):
+    right(110/180)
+    forward(Aconst * x)
+    right(1)
+    forward(1.25*x)
+    right(70/180)
+    forward(0.75 * x)
+    left(110/180)
+    forward((Aconst-1.25)*x)
+    left(1)
+    forward(Aconst * x)
+    left(70/180)
+def n(x):
+    left(1/2)
+    forward (2*x)
+    right(145/180)
+    forward(Nconst * x)
+    left(145/180)
+    forward(2*x)
+    right(1/2)
+def s(x):
+    forward(x)
+    right(1)
+    forward(x)
+    left(1/2)
+    forward(x)
+    left(1/2)
+    forward(x)
+    right(1/2)
+    forward(x)
+    right(1/2)
+    forward(x)
+    right(1)
+    forward(x)
+
+def a2(x):
+    left(70/180)
+    forward(Aconst*x)
+    left(1)
+    forward((Aconst-1.25)*x)
+    left(110/180)
+    forward(0.75 * x)
+    left(110/180)
+    forward((Aconst-1.25)*x)
+    left(1)
+    forward(Aconst*x)
+    left(70/180)
+
+
+def t(x):
     pass
 try:
     # c -> a1 -> n -> s -> a2 -> t
-    c()
-    a1()
-    n()
-    s()
-    a2()
-    t()
+    x=20
+    c(x)
+    forward(20)
+    a1(x)
+    forward(20)
+    n(x)
+    forward(20)
+    s(x)
+    forward(20)
+    a2(x)
+    forward(20)
+    t(x)
 
 finally:
     GPIO.cleanup()
